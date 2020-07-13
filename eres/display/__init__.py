0df33cd335adb3f99d07d7327f3fbf9e4c6441e5
Tharnas <8459274+Tharnas@users.noreply.github.com>
2020-07-13 11:16:33 +0100

    draw to display in an own timer

--- eres/display/__init__.py
+++ eres/display/__init__.py
@@ -2,6 +2,8 @@ import logging
 import os
 import time
 
+import threading
+
 from luma.core.interface.serial import i2c
 from luma.core.render import canvas
 from luma.oled.device import ssd1306, ssd1325, ssd1331, sh1106
@@ -20,17 +22,39 @@ class Display:
 
         super().__init__()
         self._comm = comm
-        self._state = stateMachine.state
-        self._timeLeft = "00:00"
+
+        self._displayInformation = DisplayInformation()
 
         logging.debug("Initializing display...")
 
+        # initialize display
         interface = i2c(port=1, address=0x3C)
-        currentPath = os.path.dirname(os.path.abspath(__file__))
-        self._font = os.path.join(currentPath, "arial.ttf")
         self._device = sh1106(interface, rotate=0)
 
+        # initialize fonts
+        currentPath = os.path.dirname(os.path.abspath(__file__))
+        font = os.path.join(currentPath, "arial.ttf")
+        self._smallFont = ImageFont.truetype(font, 10)
+        self._regularFont = ImageFont.truetype(font, 12)
+        self._bigFont = ImageFont.truetype(font, 40)
+
+        # start drawing timer        
+        self.runDrawTimer()
+
         logging.debug("Display initialized!")
+
+    def runDrawTimer(self):
+        self._timer = threading.Timer(0.05, self.timerCallback)
+        self._timer.start()
+
+    def timerCallback(self):
+        if self._displayInformation.shouldRedraw:
+            self.draw()
+            self._displayInformation.markDrawn()
+        
+        self._timer = threading.Timer(1, self.timerCallback)
+        self._timer.start()
+
 
     def run(self):
         logging.debug("Display: RUN!!")
@@ -42,31 +66,30 @@ class Display:
 
         return True
 
-    def print(self):
+    def draw(self):
+        startTime= time.perf_counter()
         # Box and text rendered in portrait mode
         with canvas(self._device) as draw:
-            showStart = False
-            showStop = False
-            showCountdown = False
-            font = ImageFont.truetype(self._font, 12)
+            # stop button
+            if self._displayInformation.showStopButton:
+                self.drawStop(draw, self._regularFont)
 
-            if isinstance(self._state, CountdownState):
-                stateText = "countdown"
-                showStop = True
-                showCountdown = True
-            elif isinstance(self._state, IdleState):
-                stateText = "idle"
-                showStart = True
+            # start button
+            if self._displayInformation.showStartButton:
+               self.drawStart(draw, self._regularFont)
 
-            if showStop:
-                self.drawStop(draw, font)
-            if showStart:
-                self.drawStart(draw, font)
+            # time left
+            if self._displayInformation.timeLeft:
+                self.drawCountdown(self._displayInformation.timeLeft, draw, self._bigFont)
+            
+            # status text
+            self.drawTitle(self._displayInformation.statusText, draw, self._smallFont)
 
-            if showCountdown:
-                self.drawCountdown(self._timeLeft, draw, ImageFont.truetype(self._font, 40))
-
-            self.drawTitle(stateText, draw, ImageFont.truetype(self._font, 10))
+            # status pixel
+            if self._displayInformation.statusPixel:
+                draw.rectangle((0,0,1,1), fill="white")
+        endTime = time.perf_counter()
+        logging.debug("took {} to draw".format(endTime - startTime))
 
     def drawCountdown(self, text, draw, font):
         w, h = draw.textsize(text, font=font)
@@ -101,19 +124,93 @@ class Display:
         draw.text(position, text, font=font,  fill="white")
 
     def handleState(self, state):
-        reprint = False
         if isinstance(state, State):
-            self._state = state
-            reprint = True
+            if isinstance(state, CountdownState):
+                self._displayInformation.statusText = "countdown"
+                self._displayInformation.showStartButton = False
+                self._displayInformation.showStopButton = True
+            elif isinstance(state, IdleState):
+                self._displayInformation.statusText = "idle"
+                self._displayInformation.showStartButton = True
+                self._displayInformation.showStopButton = False
+                self._displayInformation.timeLeft = ""
+
         if isinstance(state, CountdownEvent):
             if state.timeLeft > 0:
-                timeString = time.strftime("%M:%S", time.gmtime(state.timeLeft))
+                self._displayInformation.timeLeft = time.strftime("%M:%S", time.gmtime(state.timeLeft))
             else:
-                timeString = time.strftime("-%M:%S", time.gmtime(state.timeLeft*(-1)))
+                self._displayInformation.timeLeft = time.strftime("-%M:%S", time.gmtime(state.timeLeft*(-1)))
 
-            if self._timeLeft != timeString:
-                self._timeLeft = timeString
-                reprint = True
+            if (state.timeLeft % 10) == 0:
+                logging.debug("Countdown: {}".format(state.timeLeft))
+            # logging.debug("Countdown: {}".format(self._displayInformation.timeLeft))
 
-        if reprint:
-            self.print()
+
+class DisplayInformation:
+    def __init__(self):
+        super().__init__()
+
+        self._statusText = "Starting..."
+        self._timeLeft = ""
+        self._showStartButton = False
+        self._showStopButton = False
+        self._statusPixel = False
+        self._shouldRedraw = True
+
+    @property
+    def statusText(self):
+        return self._statusText
+
+    @statusText.setter
+    def statusText(self, value):
+        if value is not self._statusText:
+            self._statusText = value
+            self._shouldRedraw = True
+
+    @property
+    def statusPixel(self):
+        return self._statusPixel
+
+    @statusPixel.setter
+    def statusPixel(self, value):
+        if value is not self._statusPixel:
+            self._statusPixel = value
+            self._shouldRedraw = True
+
+    @property
+    def timeLeft(self):
+        return self._timeLeft
+
+    @timeLeft.setter
+    def timeLeft(self, value):
+        if value is not self._timeLeft:
+            self._timeLeft = value
+            self._shouldRedraw = True
+            
+    @property
+    def showStartButton(self):
+        return self._showStartButton
+
+    @showStartButton.setter
+    def showStartButton(self, value):
+        if value is not self._showStartButton:
+            self._showStartButton = value
+            self._shouldRedraw = True
+
+    @property
+    def showStopButton(self):
+        return self._showStopButton
+
+    @showStopButton.setter
+    def showStopButton(self, value):
+        if value is not self._showStopButton:
+            self._showStopButton = value
+            self._shouldRedraw = True
+
+    @property
+    def shouldRedraw(self):
+        return self._shouldRedraw
+    
+    def markDrawn(self):
+        self._shouldRedraw = False
+
